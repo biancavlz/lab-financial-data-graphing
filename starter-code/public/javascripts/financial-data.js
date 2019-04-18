9c332f6b583a15c31accaec30285f60574a1e1e3
Bianca <ing_biank@hotmail.com>
2019-04-18 12:46:19 +0200

    Axios and chart.js

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,7 +1,29 @@
+const printChart = (bpi, time) => {
+  const displayElement = document.getElementById('myChart').getContext('2d')
+  const myChart = new Chart(displayElement, {
+    type: 'line',
+      data: {
+        labels: time,
+        datasets: [
+          {
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            label: 'Stock Chart',
+            data: bpi
+          }
+        ]
+      }
+  })
+}
+
 const getBitcoinInfo = () => {
   const url = "http://api.coindesk.com/v1/bpi/historical/close.json"
   axios.get(url).then(response => {
     console.log(response)
+    const { data } = response
+    const time = Object.keys(data.bpi)
+    const bpi = Object.values(data.bpi)
+
+    printChart(bpi, time)
   }).catch(err => {
     console.error(err)
   })
